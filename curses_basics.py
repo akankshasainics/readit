4138dbd4b817f28278edcda544dfa81974b74d2b
Akanksha Saini <akankshasaini03@gmail.com>
2021-07-20 21:28:21 +0530

    Arrow Key+Enter+Backspace

--- curses_basics.py
+++ curses_basics.py
@@ -2,74 +2,88 @@ import curses
 from curses import wrapper
 max_width = 50
 new_line_char_ascii = 10
-line_end_positions = []
-user_inputs = []
-user_input = ""
+buffer = ""
 i = 0
 j = 0
 
+def get_overall_index() -> int:
+    global i
+    global j
+    index = 0
+    count = 0
+    k = 0
+    while k < len(buffer) and count < j:
+        index += 1
+        c = buffer[k]
+        if c == "\n":
+            count += 1
+        k += 1
+    index += i
+    return index
+        
+def get_length_of_prev_line() -> int:
+    global i
+    global j
+    count = 0
+    prev_line_length = 0
+    for c in buffer:
+        if c == "\n":
+            count += 1
+            if count == j+1:
+                break
+            prev_line_length = 0
+        else:
+            prev_line_length += 1
+    return prev_line_length
 
 
 def handle_backspace_key(stdscr: curses.window):
-    global user_input
+    global buffer
     global i
     global j
-    global user_inputs
-    stdscr.clear()
-    i -= 1
-    if i < 0 and j < 0:
-        i = 0
-        j = 0
-        user_input = ""
-        user_inputs = []
-        return
-    if i == -1:
-        j = j-1
-        if len(user_inputs) > 0:
-            prev_string = user_inputs.pop() 
-            i = len(prev_string)
-            user_input = prev_string + user_input
-            handle_backspace_key(stdscr)
+    if not (i == 0 and j == 0):
+        stdscr.clear()
+        i -= 1
+        if i < 0 and j < 0:
+            i = 0
+            j = 0
+            buffer = ""
             return
-    if i >= 0 and j >= 0:
-        user_input = user_input[:i] + user_input[i+1:]
-        display_prev_lines(stdscr)
-        stdscr.addstr(j,0, user_input)
+        x = get_overall_index()
+        if i < 0:
+            j -= 1
+            i = get_length_of_prev_line()
+        buffer = buffer[:x] + buffer[x+1:]
+        stdscr.addstr(0, 0, buffer)
 
-def handle_new_line():
+      
+def handle_new_line(stdscr: curses.window):
     global i
     global j
-    global user_input
-    global user_inputs
+    global buffer
+    x = get_overall_index()
+    buffer = buffer[:x] + chr(10) + buffer[x:]
+    stdscr.addstr(0, 0, buffer)
     i = 0
     j += 1
-    user_inputs.append(user_input[:i] + chr(10))
-    user_input = user_input[i:]
-
-def display_prev_lines(stdscr: curses.window):
-    global user_inputs
-    for count, user_string in enumerate(user_inputs):
-        stdscr.addstr(count, 0, user_string)
-
 
 
 def display_char(stdscr: curses.window, c: int):
     global i
     global j
-    global user_input
+    global buffer
     y,x = stdscr.getyx()
     if x == max_width:
-        handle_new_line() 
+        handle_new_line(stdscr) 
     stdscr.clear()
-    display_prev_lines(stdscr)
-    first_split = user_input[:i]
-    second_split = user_input[i:]
-    stdscr.addstr(j, 0, first_split)
-    i = len(first_split)
+    index = get_overall_index()
+    first_split = buffer[:index]
+    second_split = buffer[index:]
+    stdscr.addstr(0, 0, first_split)
     stdscr.addch(j, i, chr(c))
     i += 1
     stdscr.addstr(j, i, second_split)
-    user_input = first_split + chr(c) + second_split
+    buffer = first_split + chr(c) + second_split
 
 def handle_arrow_right_key(stdscr: curses.window):
     global i
@@ -84,7 +98,7 @@ def handle_arrow_left_key(stdscr: curses.window):
     stdscr.move(j, i)
    
 def main(stdscr: curses.window):
-    global user_input
+    global buffer
     global i
     global j
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_CYAN)
@@ -100,7 +114,7 @@ def main(stdscr: curses.window):
             elif c == 261:
                 handle_arrow_right_key(stdscr)
             elif c == 10:
-                handle_new_line()
+                handle_new_line(stdscr)
             else:
                 display_char(stdscr, c)
             stdscr.move(j, i) 
